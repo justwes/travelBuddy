6c0b269b2e79f70b1e42104952bad79554e7046a
Wes C <justwes@gmail.com>
2017-06-04 10:11:30 +0100

    Initial commit

---
+++ apps/login/views.py
@@ -0,0 +1,52 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from models import User
+
+# Create your views here.
+
+
+def session_check(request):
+    if 'user' in request.session:
+        return True
+    else:
+        return False
+
+
+def index(request):
+    if session_check(request):
+        return redirect('travel_buddy:index')
+    else:
+        return render(request, 'login/index.html')
+
+
+def login_reg(request):
+    if request.POST['action'] == 'register':
+        result = User.objects.validate_reg(request)
+    elif request.POST['action'] == 'login':
+        result = User.objects.validate_login(request)
+    if result[0] == False:
+        print_errors(request, result[1])
+        return redirect('login:index')
+    return log_user_in(request, result[1])
+
+
+def print_errors(request, error_list):
+    for error in error_list:
+        messages.add_message(request, messages.INFO, error)
+
+
+def log_user_in(request, user):
+    request.session['user'] = {
+        'user_id': user.id,
+        'name': user.name
+    }
+    return redirect('travel_buddy:index')
+
+
+def logout(request):
+    request.session.clear()
+
+    return redirect('login:index')
